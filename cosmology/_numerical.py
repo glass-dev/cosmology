0793d8ef5a1e3ea1ed31a3fe107f764a2d23b4c2
Nicolas Tessore <n.tessore@ucl.ac.uk>
2021-10-01 15:48:57 +0100

    invert cubic polynomial for inverse functions

--- cosmology/_numerical.py
+++ cosmology/_numerical.py
@@ -3,6 +3,7 @@
 '''numerical methods'''
 
 import numpy as np
+from numpy.polynomial.polynomial import polyval, polyroots
 from functools import partial
 
 
@@ -66,7 +67,32 @@ def cubic(xi, x, y, yp):
     c = fp0
     d = f0
 
-    return ((a*t + b)*t + c)*t + d
+    return polyval(t, (d, c, b, a), False)
+
+
+def cubic_inv(yi, x, y, yp):
+    '''inverse cubic interpolation given derivatives'''
+    i1 = np.digitize(yi, y)
+    i0 = i1-1
+
+    x0, x1 = x[i0], x[i1]
+    dx = x1 - x0
+
+    f0, f1 = y[i0], y[i1]
+    fp0, fp1 = dx*yp[i0], dx*yp[i1]
+
+    a = 2*f0 - 2*f1 + fp0 + fp1
+    b = -3*f0 + 3*f1 - 2*fp0 - fp1
+    c = fp0
+    d = f0
+
+    # find t such that yi == at^3 + bt^2 + ct + d
+    t = np.empty_like(yi)
+    for k in np.ndindex(t.shape):
+        r = polyroots((d[k] - yi[k], c[k], b[k], a[k]))
+        t[k] = r[(r.imag == 0.) & (0 <= r.real) & (r.real < 1)].real
+
+    return x0 + t*dx
 
 
 def antideriv(f, x0, x1, h, *, c=0., atol=1e-8, rtol=1e-8, inverse=False):
@@ -85,9 +111,9 @@ def antideriv(f, x0, x1, h, *, c=0., atol=1e-8, rtol=1e-8, inverse=False):
     if inverse:
         dy = np.diff(y)
         if np.all(dy > 0):
-            ad_inv = partial(cubic, x=y, y=x, yp=1/yp)
+            ad_inv = partial(cubic_inv, x=x, y=y, yp=yp)
         elif np.all(dy < 0):
-            ad_inv = partial(cubic, x=y[::-1], y=x[::-1], yp=1/yp[::-1])
+            ad_inv = partial(cubic_inv, x=x[::-1], y=y[::-1], yp=yp[::-1])
         else:
             raise ValueError('antiderivative not invertible')
 
